b95c8eb959fec59afa9b57ae38477c0061f7319a
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-08-28 01:06:09 +0200

    v0.3

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
+import os
+import sys
 from flask import Flask
+
+path = '/sys/bus/w1/devices/'
+if not os.path.exists(path):
+    sys.stderr.write('The /sys/bus/w1/devices not exists. Is GPIO enabled ?\n')
 
 app = Flask(__name__)
 
